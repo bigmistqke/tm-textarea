bd93363266e8df953e58998bdc187c29e745d1a2
bigmistqke <bigmistqke@users.noreply.github.com>
2024-09-23 16:19:30 +0000

    deploy: 434c1c385a55fb87f5cc0179e37973b89a0e8004

--- assets/index-BBknzvmN.js
+++ assets/index-BBknzvmN.js
@@ -287,9 +287,6 @@ function createSelector(source, fn = equalFn, options) {
     return fn(key, node.value);
   };
 }
-function batch(fn) {
-  return runUpdates(fn, false);
-}
 function untrack(fn) {
   if (Listener === null) return fn();
   const listener = Listener;
@@ -2913,220 +2910,6 @@ function createLazyMemo(calc, value, options) {
 
 function r(e){var t,f,n="";if("string"==typeof e||"number"==typeof e)n+=e;else if("object"==typeof e)if(Array.isArray(e)){var o=e.length;for(t=0;t<o;t++)e[t]&&(f=r(e[t]))&&(n&&(n+=" "),n+=f);}else for(f in e)e[f]&&(n&&(n+=" "),n+=f);return n}function clsx(){for(var e,t,f=0,n="",o=arguments.length;f<o;f++)(e=arguments[f])&&(t=r(e))&&(n&&(n+=" "),n+=t);return n}
 
-const $RAW = Symbol("store-raw"),
-  $NODE = Symbol("store-node"),
-  $HAS = Symbol("store-has"),
-  $SELF = Symbol("store-self");
-function wrap$1(value) {
-  let p = value[$PROXY];
-  if (!p) {
-    Object.defineProperty(value, $PROXY, {
-      value: p = new Proxy(value, proxyTraps$1)
-    });
-    if (!Array.isArray(value)) {
-      const keys = Object.keys(value),
-        desc = Object.getOwnPropertyDescriptors(value);
-      for (let i = 0, l = keys.length; i < l; i++) {
-        const prop = keys[i];
-        if (desc[prop].get) {
-          Object.defineProperty(value, prop, {
-            enumerable: desc[prop].enumerable,
-            get: desc[prop].get.bind(p)
-          });
-        }
-      }
-    }
-  }
-  return p;
-}
-function isWrappable(obj) {
-  let proto;
-  return obj != null && typeof obj === "object" && (obj[$PROXY] || !(proto = Object.getPrototypeOf(obj)) || proto === Object.prototype || Array.isArray(obj));
-}
-function unwrap(item, set = new Set()) {
-  let result, unwrapped, v, prop;
-  if (result = item != null && item[$RAW]) return result;
-  if (!isWrappable(item) || set.has(item)) return item;
-  if (Array.isArray(item)) {
-    if (Object.isFrozen(item)) item = item.slice(0);else set.add(item);
-    for (let i = 0, l = item.length; i < l; i++) {
-      v = item[i];
-      if ((unwrapped = unwrap(v, set)) !== v) item[i] = unwrapped;
-    }
-  } else {
-    if (Object.isFrozen(item)) item = Object.assign({}, item);else set.add(item);
-    const keys = Object.keys(item),
-      desc = Object.getOwnPropertyDescriptors(item);
-    for (let i = 0, l = keys.length; i < l; i++) {
-      prop = keys[i];
-      if (desc[prop].get) continue;
-      v = item[prop];
-      if ((unwrapped = unwrap(v, set)) !== v) item[prop] = unwrapped;
-    }
-  }
-  return item;
-}
-function getNodes(target, symbol) {
-  let nodes = target[symbol];
-  if (!nodes) Object.defineProperty(target, symbol, {
-    value: nodes = Object.create(null)
-  });
-  return nodes;
-}
-function getNode(nodes, property, value) {
-  if (nodes[property]) return nodes[property];
-  const [s, set] = createSignal(value, {
-    equals: false,
-    internal: true
-  });
-  s.$ = set;
-  return nodes[property] = s;
-}
-function proxyDescriptor$1(target, property) {
-  const desc = Reflect.getOwnPropertyDescriptor(target, property);
-  if (!desc || desc.get || !desc.configurable || property === $PROXY || property === $NODE) return desc;
-  delete desc.value;
-  delete desc.writable;
-  desc.get = () => target[$PROXY][property];
-  return desc;
-}
-function trackSelf(target) {
-  getListener() && getNode(getNodes(target, $NODE), $SELF)();
-}
-function ownKeys(target) {
-  trackSelf(target);
-  return Reflect.ownKeys(target);
-}
-const proxyTraps$1 = {
-  get(target, property, receiver) {
-    if (property === $RAW) return target;
-    if (property === $PROXY) return receiver;
-    if (property === $TRACK) {
-      trackSelf(target);
-      return receiver;
-    }
-    const nodes = getNodes(target, $NODE);
-    const tracked = nodes[property];
-    let value = tracked ? tracked() : target[property];
-    if (property === $NODE || property === $HAS || property === "__proto__") return value;
-    if (!tracked) {
-      const desc = Object.getOwnPropertyDescriptor(target, property);
-      if (getListener() && (typeof value !== "function" || target.hasOwnProperty(property)) && !(desc && desc.get)) value = getNode(nodes, property, value)();
-    }
-    return isWrappable(value) ? wrap$1(value) : value;
-  },
-  has(target, property) {
-    if (property === $RAW || property === $PROXY || property === $TRACK || property === $NODE || property === $HAS || property === "__proto__") return true;
-    getListener() && getNode(getNodes(target, $HAS), property)();
-    return property in target;
-  },
-  set() {
-    return true;
-  },
-  deleteProperty() {
-    return true;
-  },
-  ownKeys: ownKeys,
-  getOwnPropertyDescriptor: proxyDescriptor$1
-};
-function setProperty(state, property, value, deleting = false) {
-  if (!deleting && state[property] === value) return;
-  const prev = state[property],
-    len = state.length;
-  if (value === undefined) {
-    delete state[property];
-    if (state[$HAS] && state[$HAS][property] && prev !== undefined) state[$HAS][property].$();
-  } else {
-    state[property] = value;
-    if (state[$HAS] && state[$HAS][property] && prev === undefined) state[$HAS][property].$();
-  }
-  let nodes = getNodes(state, $NODE),
-    node;
-  if (node = getNode(nodes, property, prev)) node.$(() => value);
-  if (Array.isArray(state) && state.length !== len) {
-    for (let i = state.length; i < len; i++) (node = nodes[i]) && node.$();
-    (node = getNode(nodes, "length", len)) && node.$(state.length);
-  }
-  (node = nodes[$SELF]) && node.$();
-}
-function mergeStoreNode(state, value) {
-  const keys = Object.keys(value);
-  for (let i = 0; i < keys.length; i += 1) {
-    const key = keys[i];
-    setProperty(state, key, value[key]);
-  }
-}
-function updateArray(current, next) {
-  if (typeof next === "function") next = next(current);
-  next = unwrap(next);
-  if (Array.isArray(next)) {
-    if (current === next) return;
-    let i = 0,
-      len = next.length;
-    for (; i < len; i++) {
-      const value = next[i];
-      if (current[i] !== value) setProperty(current, i, value);
-    }
-    setProperty(current, "length", len);
-  } else mergeStoreNode(current, next);
-}
-function updatePath(current, path, traversed = []) {
-  let part,
-    prev = current;
-  if (path.length > 1) {
-    part = path.shift();
-    const partType = typeof part,
-      isArray = Array.isArray(current);
-    if (Array.isArray(part)) {
-      for (let i = 0; i < part.length; i++) {
-        updatePath(current, [part[i]].concat(path), traversed);
-      }
-      return;
-    } else if (isArray && partType === "function") {
-      for (let i = 0; i < current.length; i++) {
-        if (part(current[i], i)) updatePath(current, [i].concat(path), traversed);
-      }
-      return;
-    } else if (isArray && partType === "object") {
-      const {
-        from = 0,
-        to = current.length - 1,
-        by = 1
-      } = part;
-      for (let i = from; i <= to; i += by) {
-        updatePath(current, [i].concat(path), traversed);
-      }
-      return;
-    } else if (path.length > 1) {
-      updatePath(current[part], path, [part].concat(traversed));
-      return;
-    }
-    prev = current[part];
-    traversed = [part].concat(traversed);
-  }
-  let value = path[0];
-  if (typeof value === "function") {
-    value = value(prev, traversed);
-    if (value === prev) return;
-  }
-  if (part === undefined && value == undefined) return;
-  value = unwrap(value);
-  if (part === undefined || isWrappable(prev) && isWrappable(value) && !Array.isArray(value)) {
-    mergeStoreNode(prev, value);
-  } else setProperty(current, part, value);
-}
-function createStore(...[store, options]) {
-  const unwrappedStore = unwrap(store || {});
-  const isArray = Array.isArray(unwrappedStore);
-  const wrappedStore = wrap$1(unwrappedStore);
-  function setStore(...args) {
-    batch(() => {
-      isArray && args.length === 1 ? updateArray(unwrappedStore, args[0]) : updatePath(unwrappedStore, args);
-    });
-  }
-  return [wrappedStore, setStore];
-}
-
 var commonjsGlobal = typeof globalThis !== 'undefined' ? globalThis : typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : {};
 
 function getDefaultExportFromCjs (x) {
@@ -5264,7 +5047,7 @@ class SegmentManager {
     this.tokenizer = tokenizer;
     this.theme = theme;
     this.source = source;
-    [this.#segments, this.#setSegments] = createStore([]);
+    [this.#segments, this.#setSegments] = createSignal([]);
     this.lines = createMemo(() => source().split("\n"));
     const owner = getOwner();
     createRenderEffect(() => {
@@ -5272,7 +5055,7 @@ class SegmentManager {
       const newSegmentCount = Math.ceil(newLineCount / this.segmentSize);
       const currentSegmentCount = this.#segments.length;
       if (newSegmentCount > currentSegmentCount) {
-        let previousSegment = this.#segments[this.#segments.length - 1] || null;
+        let previousSegment = this.#segments()[this.#segments.length - 1] || null;
         for (let i = currentSegmentCount; i < newSegmentCount; i += 1) {
           const segment = runWithOwner(owner, () => new Segment(this, previousSegment, i));
           this.#setSegments((prev) => [...prev, segment]);
@@ -5289,11 +5072,11 @@ class SegmentManager {
   segmentSize = SEGMENT_SIZE;
   lines;
   getSegment(index) {
-    return this.#segments[index] || void 0;
+    return this.#segments()[index] || void 0;
   }
   getLine(globalOffset) {
     const segmentIndex = Math.floor(globalOffset / this.segmentSize);
-    const segment = this.#segments[segmentIndex];
+    const segment = this.#segments()[segmentIndex];
     if (!segment) {
       return void 0;
     }

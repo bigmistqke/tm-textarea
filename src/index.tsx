0071fb2286addb30d10cfd43a79695d2ae08b2b2
bigmistqke <vandijckv@gmail.com>
2024-09-27 20:30:37 +0200

    core: add textarea specific methods and properties see https://github.com/bigmistqke/tm-textarea/issues/3
    - move jsx to constructor, so we have access to textarea immediately.
    - add `self = this` to prevent transform-bug
    - add methods/properties:
        - selectionStart
        - selectionEnd
        - setRangeText()
        - setSelectionRange()
        - select()

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,8 @@ import {
   ElementAttributes,
   stringAttribute,
 } from '@lume/element'
+import { signal } from 'classy-solid'
+import { JSXElement } from 'solid-js'
 import { createTmTextarea, css } from './core'
 import classnames from './index.module.css?classnames'
 import { Grammar, Theme } from './tm'
@@ -52,12 +54,38 @@ const TmTextarea = createTmTextarea(Object.fromEntries(classnames.map(name => [n
 const TmTextareaStyleSheet = sheet(css)
 
 @element('tm-textarea')
-class TmTextareaElement extends Element {
+export class TmTextareaElement extends Element {
   @booleanAttribute editable = true
   @stringAttribute grammar: Grammar = 'tsx'
   @stringAttribute stylesheet = ''
   @stringAttribute theme: Theme = 'dark-plus'
   @stringAttribute value = ''
+  @signal textarea: HTMLTextAreaElement = null!
+  @signal jsx: JSXElement | undefined
+  @signal #finalize
+
+  constructor() {
+    super()
+
+    // `self` is necessary otherwise, otherwise the solid-transform would place it before super()
+    const self = this
+
+    // Defining jsx in the constructor so TmTextareaElement.textarea is defined.
+    // Otherwise methods relying on textarea will not work.
+    this.jsx = (
+      <TmTextarea
+        textareaRef={element => {
+          element.value = self.value
+          self.textarea = element
+        }}
+        grammar={self.grammar}
+        theme={self.theme}
+        value={self.value}
+        editable={self.editable}
+        onInput={e => (self.value = e.currentTarget.value)}
+      />
+    )
+  }
 
   template = () => {
     const adoptedStyleSheets = this.shadowRoot!.adoptedStyleSheets
@@ -67,16 +95,43 @@ class TmTextareaElement extends Element {
     if (this.stylesheet) {
       adoptedStyleSheets.push(sheet(this.stylesheet))
     }
+    return this.jsx
+  }
 
-    return (
-      <TmTextarea
-        grammar={this.grammar}
-        theme={this.theme}
-        value={this.value}
-        editable={this.editable}
-        onInput={e => (this.value = e.currentTarget.value)}
-      />
-    )
+  get selectionStart() {
+    return this.textarea.selectionStart
+  }
+  set selectionStart(start: number) {
+    this.textarea.selectionStart = start
+  }
+  get selectionEnd() {
+    return this.textarea.selectionEnd
+  }
+  set selectionEnd(end: number) {
+    this.textarea.selectionEnd = end
+  }
+
+  setRangeText(
+    replacement: string,
+    start?: number,
+    end?: number,
+    selectMode?: 'select' | 'start' | 'end' | 'preserve',
+  ) {
+    // @ts-expect-error due to method overloads
+    this.textarea.setRangeText(replacement, start, end, selectMode)
+    this.value = this.textarea.value
+  }
+
+  setSelectionRange(
+    selectionStart: number,
+    selectionEnd: number,
+    selectionDirection?: 'forward' | 'backward' | 'none',
+  ) {
+    this.textarea.setSelectionRange(selectionStart, selectionEnd, selectionDirection)
+  }
+
+  select() {
+    this.textarea.select()
   }
 }
 
